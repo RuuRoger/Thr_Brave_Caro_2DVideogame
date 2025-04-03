c843e30ee995471847e490a18efc6019784d2ef7
RuuRoger <lozanofierroruu@gmail.com>
2025-04-04 00:15:40 +0200

    Se ha terminado las funcionalidades de settings y se ha agregado música para el menú

--- Assets/Scripts/Buttons/UIMenuContrller.cs
+++ Assets/Scripts/Buttons/UIMenuContrller.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using System;
+using UnityEngine.Rendering;
 
 public class UIMenuContrller : MonoBehaviour
 {
@@ -13,6 +14,13 @@ public class UIMenuContrller : MonoBehaviour
     [SerializeField] private Button _cancelButtonObject;
     [SerializeField] private GameObject _seettingsPanel;
     [SerializeField] private TMP_Dropdown _qualityDrop;
+    [SerializeField] private Toggle _vSyncToggle;
+    [SerializeField] private Toggle _fullScreenToggle;
+    [SerializeField] private Toggle _noShadowToggle;
+    [SerializeField] private Toggle _softShadowToggle;
+    [SerializeField] private Toggle _hardShadowToggle;
+    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private AudioSource _audioSource;
 
     #endregion
 
@@ -27,11 +35,13 @@ public class UIMenuContrller : MonoBehaviour
         _seettingsPanel.gameObject.SetActive(true);
 
         //Active quality drop
-        QualitySettings();
+        QualitySettingsDrop();
 
         //Diseable button options
         gameObject.SetActive(false);
     }
+
+    private void ExitGame() => Application.Quit();
 
     private void CloseSettingPanel()
     {
@@ -44,7 +54,7 @@ public class UIMenuContrller : MonoBehaviour
         _seettingsPanel.gameObject.SetActive(false);
     }
 
-    private void QualitySettings()
+    private void QualitySettingsDrop()
     {
         //Take vaues for quality settings
         _qualityDrop.ClearOptions();
@@ -62,14 +72,53 @@ public class UIMenuContrller : MonoBehaviour
         UnityEngine.QualitySettings.SetQualityLevel(arg0, true);
     }
 
+    private void SetVSync(bool on)
+    {
+        if (on)
+            QualitySettings.vSyncCount = 1;
+        else
+            QualitySettings.vSyncCount = 0;
+    }
+
+    private void SetFullScreen(bool on) => Screen.fullScreen = !Screen.fullScreen;
+
+    private void SetVolume(float value) => _audioSource.volume = value;
+
+    private void SetNoShadows(bool on)
+    {
+        if (on)
+            QualitySettings.shadows = ShadowQuality.Disable;
+    }
+
+    private void SetSoftShadows(bool on)
+    {
+        if (on)
+           QualitySettings.shadows  = ShadowQuality.All;        
+    }
+
+    private void SetHardShadows(bool on)
+    {
+        if (on)
+            QualitySettings.shadows = ShadowQuality.HardOnly;
+    }
+
     #endregion
 
     #region Unity Callbacks
     private void Start()
     {
         this.gameObject.GetComponent<Button>().onClick.AddListener(ClickOptionsButton);
+        _exitButtonObject.GetComponent<Button>().onClick.AddListener(ExitGame);
         _qualityDrop.onValueChanged.AddListener(SetQuality);
         _cancelButtonObject.onClick.AddListener(CloseSettingPanel);
+        _vSyncToggle.onValueChanged.AddListener(SetVSync);
+        _fullScreenToggle.onValueChanged.AddListener(SetFullScreen);
+        _noShadowToggle.onValueChanged.AddListener(SetNoShadows);
+        _softShadowToggle.onValueChanged.AddListener(SetSoftShadows);
+        _hardShadowToggle.onValueChanged.AddListener(SetHardShadows);
+
+        volumeSlider.value = _audioSource.volume;
+        volumeSlider.onValueChanged.AddListener(SetVolume);
     }
 
     #endregion
